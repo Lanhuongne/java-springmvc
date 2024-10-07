3b2efc9e91c61f211aeae5c8924a9a1cc298c8b3
lan Huong <Lehuong420@gmail.com>
2024-10-08 00:09:20 +0700

    updateupdate1008

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "ahihi";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String AdminPage() {
+        return "only admin can access this page";
     }
 }
